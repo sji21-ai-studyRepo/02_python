543a5304ece819e04c0b91f539c930c825cfee9b
sji21 <sr19960229@gmail.com>
2026-05-29 17:51:04 +0900

    20260529_데이터타입 (number, bool)

---
+++ _02_data_type/_02_bool.py
@@ -0,0 +1,25 @@
+# 논리형 Boolean
+
+a = True
+b = False
+print(a, type(a))
+print(b, type(b))
+
+# 비교 연산
+# A > B : A가 B보다 크면 True, 아니면 False
+# A >= B : A가 B 이상이면 Tre, 아니면 False
+# A < B
+# A <= B
+# A ==B : A, B가 같으면 True, 아니면 False
+# A != B : A, B가 같지 않으면 True
+print("1 > 0.5:", 1 > 0.5) # T
+print("1 < 0.5:", 1 < 0.5) # F
+print("1 >= 0.5:", 1 >= 0.5) # T
+print("1 <= 0.5:", 1 <= 0.5) # F
+print("1 == 1:", 1 == 1) # T
+print("1 != 1:", 1 != 1) # F
+
+# 논리 부정 연산(not)
+print(True)
+print(not(True))
+print(not not True)
